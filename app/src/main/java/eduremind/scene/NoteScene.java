69be7f763ee8697685a35481def459b1e43ebadd
FailFudhayl <ahmadfudhayl06@gmail.com>
2023-05-24 21:53:58 +0800

    feat : membuat note scene

---
+++ app/src/main/java/eduremind/scene/NoteScene.java
@@ -0,0 +1,15 @@
+package eduremind.scene;
+
+import javafx.stage.Stage;
+
+public class NoteScene {
+    private Stage stage;
+
+    public SplashScene(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void show() {
+
+    }
+}
