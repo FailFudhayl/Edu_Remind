6298f8d0fbf3402d227357f41a821037085aa522
FailFudhayl <ahmadfudhayl06@gmail.com>
2023-05-24 21:29:04 +0800

    feat : membuat stage dan splash scene

---
+++ app/src/main/java/eduremind/scene/SplashScene.java
@@ -0,0 +1,15 @@
+package eduremind.scene;
+
+import javafx.stage.Stage;
+
+public class SplashScene {
+    private Stage stage;
+
+    public SplashScene(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void show() {
+
+    }
+}
