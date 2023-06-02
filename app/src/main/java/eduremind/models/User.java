d5ed9ca81bd837b8fcb62b4a6e5a09dc3b68e7bf
FailFudhayl <ahmadfudhayl06@gmail.com>
2023-06-02 20:20:51 +0800

    feat : buat class model untuk user

--- app/src/main/java/eduremind/models/User.java
+++ app/src/main/java/eduremind/models/User.java
@@ -1,34 +1,32 @@
-// package eduremind.models;
+package eduremind.models;
 
-// public class User {
-//     private int id;
-//     private String name;
-//     private String pass;
+public class User {
+    private int id;
+    private String name;
+    private String pass;
     
-//     public User(int id, String name, String pass) {
-//         this.id = id;
-//         this.name = name;
-//         this.pass = pass;
-//     }
+    // public User(int id, String name, String pass) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.pass = pass;
+    // }
     
-//     public int getId() {
-//         return id;
-//     }
-//     public void setId(int id) {
-//         this.id = id;
-//     }
-//     public String getName() {
-//         return name;
-//     }
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-//     public String getPass() {
-//         return pass;
-//     }
-//     public void setPass(String pass) {
-//         this.pass = pass;
-//     }
-    
-    
-// }
+    // public int getId() {
+    //     return id;
+    // }
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+    // public String getName() {
+    //     return name;
+    // }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+    // public String getPass() {
+    //     return pass;
+    // }
+    // public void setPass(String pass) {
+    //     this.pass = pass;
+    // }
+}
