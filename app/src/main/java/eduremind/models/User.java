10ae83b579a7a7f6df80b569c549ec909364e4d2
FailFudhayl <ahmadfudhayl06@gmail.com>
2023-06-02 20:22:01 +0800

    feat : buat setter getter untuk kelas user

--- app/src/main/java/eduremind/models/User.java
+++ app/src/main/java/eduremind/models/User.java
@@ -11,22 +11,22 @@ public class User {
         this.pass = pass;
     }
     
-    // public int getId() {
-    //     return id;
-    // }
-    // public void setId(int id) {
-    //     this.id = id;
-    // }
-    // public String getName() {
-    //     return name;
-    // }
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-    // public String getPass() {
-    //     return pass;
-    // }
-    // public void setPass(String pass) {
-    //     this.pass = pass;
-    // }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
