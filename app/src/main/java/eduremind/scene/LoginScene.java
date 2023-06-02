2122e1548aefd0ae978ddc3c6fe5d61a5989762a
FailFudhayl <ahmadfudhayl06@gmail.com>
2023-06-02 20:31:34 +0800

    feat : masukkan logo dalam hbox

--- app/src/main/java/eduremind/scene/LoginScene.java
+++ app/src/main/java/eduremind/scene/LoginScene.java
@@ -41,8 +41,8 @@ public class LoginScene {
         loginLogos.setFitHeight(150);
         //git commit -m "feat : setID logo"
         loginLogos.setId("logo1");
-//         //git commit -m feat : "masukkan logo dalam hbox"
-//         HBox logoBox = new HBox(loginLogos);
+        //git commit -m "feat : masukkan logo dalam hbox"
+        HBox logoBox = new HBox(loginLogos);
 //         //git commit -m feat : "atur agar logobox di tengah lalu letakkan di atas"
 //         logoBox.setAlignment(Pos.CENTER);
 //         root.setTop(logoBox);
